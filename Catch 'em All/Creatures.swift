210ac39ddca2fd28c5641cb9a616f5ace7bc5a21
Kevin Watke <kjwatke@gmail.com>
2022-03-15 22:55:09 -0500

    fixed error with fast scrolling causing duplicate api calls

--- Catch 'em All/Creatures.swift
+++ Catch 'em All/Creatures.swift
@@ -19,10 +19,13 @@ class Creatures {
 	var count = 0
 	var urlString = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20"
 	var creatureArray: [Creature] = []
+	var isFetching = false
 	
 	
 	func getData(completed: @escaping () -> () ) {
-		print("Accessing URL: \(urlString)")
+		guard !isFetching else { return }
+		isFetching = true
+	
 		guard let url = URL(string: urlString) else { return }
 		
 		let session = URLSession.shared
@@ -34,13 +37,14 @@ class Creatures {
 			
 			do {
 				let returned = try JSONDecoder().decode(Returned.self, from: data!)
-				print("Returned: \(returned)")
+		
 				self.creatureArray += returned.results
 				self.urlString = returned.next ?? ""
 				self.count = returned.count
 			} catch  {
 				print("Error creating the data: \(error)")
 			}
+			self.isFetching = false
 			completed()
 		}
 		task.resume()
